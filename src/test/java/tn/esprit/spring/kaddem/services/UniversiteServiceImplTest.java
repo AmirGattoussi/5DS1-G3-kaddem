f79e1df71e69db34378de78003d47e1b97fb448d
AmirGattoussi <Amir.GattoussiLahouel@esprit.tn>
2024-11-01 17:27:37 +0100

    TEST mockito

--- src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
@@ -1,46 +1,36 @@
-//package tn.esprit.spring.kaddem.services;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import tn.esprit.spring.kaddem.entities.Universite;
-//import tn.esprit.spring.kaddem.services.IUniversiteService;
-//
-//import java.text.ParseException;
-//
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//@RunWith(SpringRunner.class)
-//
-//@SpringBootTest
-//
-//@Slf4j
-//
-//class UniversiteServiceImplTest {
-//
-//    @Autowired
-//
-//    IUniversiteService universiteService;
-//    @Test
-//
-//     void testAddUniversite() throws ParseException {
-//
-//
-//
-//
-//        Universite u = new Universite("ESPRIT");
-//
-//        Universite universite = universiteService.addUniversite(u);
-//
-//        log.info("Universite "+ universite);
-//
-//        assertNotNull(universite.getIdUniv());
-//
-//        assertNotNull(universite.getNomUniv());
-//
-//
-//
-//
-//    }}
+package tn.esprit.spring.kaddem.services;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.kaddem.entities.Universite;
+import tn.esprit.spring.kaddem.repositories.UniversiteRepository;
+class UniversiteServiceImplTest {
+
+    @InjectMocks
+    private UniversiteServiceImpl universiteService;
+
+    @Mock
+    private UniversiteRepository universiteRepository; // Assuming you have a repository interface
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testAddUniversite() {
+        Universite u = new Universite("ESPRIT");
+        when(universiteRepository.save(u)).thenReturn(u); // Mocking the save method
+
+        Universite result = universiteService.addUniversite(u);
+
+        assertNotNull(result);
+        assertEquals("ESPRIT", result.getNomUniv());
+        verify(universiteRepository, times(1)).save(u); // Verify that save was called
+    }
+}
