d9862a735729be93d164f3a28795aad3b803a766
AmirGattoussi <Amir.GattoussiLahouel@esprit.tn>
2024-11-03 02:11:53 +0100

    DTO test @Data coverage

--- src/test/java/tn/esprit/spring/kaddem/dto/UniversiteDTOTest.java
+++ src/test/java/tn/esprit/spring/kaddem/dto/UniversiteDTOTest.java
@@ -2,49 +2,62 @@ package tn.esprit.spring.kaddem.dto;
 
 import org.junit.jupiter.api.Test;
 
+
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-class UniversiteDTOTest {
+public class UniversiteDTOTest {
 
     @Test
-    void testUniversiteDTO() {
-        // Create UniversiteDTO instance using Lombok-generated constructor
+    public void testUniversiteDTO() {
+        // Create UniversiteDTO instance
         UniversiteDTO universiteDTO = new UniversiteDTO("Test University");
 
-        // Verify the field values
-        assertNotNull(universiteDTO);
+        // Verify the field value
         assertEquals("Test University", universiteDTO.getNomUniv());
 
-        // Modify the value to test setters
+        // Test setter and getter
         universiteDTO.setNomUniv("Updated University");
         assertEquals("Updated University", universiteDTO.getNomUniv());
+
+        // Test toString
+        assertNotNull(universiteDTO.toString());
+
+        // Test equals and hashCode
+        UniversiteDTO universiteDTO2 = new UniversiteDTO("Updated University");
+        assertEquals(universiteDTO, universiteDTO2);
+        assertEquals(universiteDTO.hashCode(), universiteDTO2.hashCode());
     }
 
     @Test
-    void testUpdateUniversiteDTO() {
+    public void testUpdateUniversiteDTO() {
         // Create a set of department IDs
         Set<Integer> departmentIds = new HashSet<>();
         departmentIds.add(1);
         departmentIds.add(2);
 
-        // Create UpdateUniversiteDTO instance using Lombok-generated constructor
+        // Create UpdateUniversiteDTO instance
         UpdateUniversiteDTO updateUniversiteDTO = new UpdateUniversiteDTO(1, "Updated University", departmentIds);
 
         // Verify the field values
-        assertNotNull(updateUniversiteDTO);
         assertEquals(1, updateUniversiteDTO.getIdUniv());
         assertEquals("Updated University", updateUniversiteDTO.getNomUniv());
         assertEquals(departmentIds, updateUniversiteDTO.getDepartementIds());
 
-        // Modify the values to test setters
+        // Test setters and getters
         updateUniversiteDTO.setNomUniv("New Name");
         updateUniversiteDTO.setIdUniv(2);
-
         assertEquals("New Name", updateUniversiteDTO.getNomUniv());
         assertEquals(2, updateUniversiteDTO.getIdUniv());
+
+        // Test toString
+        assertNotNull(updateUniversiteDTO.toString());
+
+        // Test equals and hashCode
+        UpdateUniversiteDTO updateUniversiteDTO2 = new UpdateUniversiteDTO(2, "New Name", departmentIds);
+        assertEquals(updateUniversiteDTO, updateUniversiteDTO2);
+        assertEquals(updateUniversiteDTO.hashCode(), updateUniversiteDTO2.hashCode());
     }
 }
